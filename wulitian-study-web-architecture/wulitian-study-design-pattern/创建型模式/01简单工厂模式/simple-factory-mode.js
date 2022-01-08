413ab94ce667b934892837b55079483354172848
13344 <1334416530@qq.com>
2022-01-08 22:58:59 +0800

    设计模式之三种工厂模式整理

--- wulitian-study-web-architecture/wulitian-study-design-pattern/创建型模式/01简单工厂模式/simple-factory-mode.js
+++ wulitian-study-web-architecture/wulitian-study-design-pattern/创建型模式/01简单工厂模式/simple-factory-mode.js
@@ -2,34 +2,107 @@
  * 简单工厂模式示例
  */
 
-function Factory(name, type, time){
-    let obj = new Object();
+/*-----------------------------------------*/
+function Factory(name, type, time) {
+    let obj = {};
     obj.name = name;
     obj.type = type;
     obj.time = time;
     let objType = {
-        'js':()=>{
+        'js': () => {
             obj.js = 'js';
             return obj
         },
-        'java':()=>{
+        'java': () => {
             obj.java = 'java';
             return obj
         },
-        'vb':()=>{
+        'vb': () => {
             obj.vb = 'vb';
             return obj
         }
     }
-    console.log(objType[name]())
-    objType[name]||objType[name]();
+    console.log(objType[name]());
+    objType[name] || objType[name]();
     return obj
 }
 
-let book1 = new Factory('js','雾里天','2021-09-17');
-let book2 = new Factory('java','雾里天','2021-09-17');
-let book3 = new Factory('vb','雾里天','2021-09-17');
+let book1 = new Factory('js', '雾里天', '2021-09-17');
+let book2 = new Factory('java', '雾里天', '2021-09-17');
+let book3 = new Factory('vb', '雾里天', '2021-09-17');
 
-console.log(book1)
-console.log(book2)
-console.log(book3)
+console.log(book1);
+console.log(book2);
+console.log(book3);
+
+/*-----------------------------------------*/
+function Car(val) {
+    this.name = val;
+    this.run = () => {
+        console.log(this.name);
+    }
+}
+
+function Airplane(val) {
+    this.name = val;
+    this.air = () => {
+        console.log(this.name);
+    }
+}
+
+function Factory2(val) {
+    switch (val) {
+        case("Car"):
+            return new Car(val);
+        case("Airplane"):
+            return new Airplane(val);
+    }
+    return {};
+}
+
+let carInstance = new Factory2("Car");
+let airplaneInstance = new Factory2("Airplane");
+console.log(carInstance);
+console.log(airplaneInstance);
+carInstance.run();
+airplaneInstance.air();
+
+/*-----------------------------------------*/
+class Cat {
+    constructor() {
+        this.name = '猫';
+    }
+
+    eat() {
+        console.log('猫吃饭');
+    }
+}
+
+class Dog {
+    constructor() {
+        this.name = "狗";
+    }
+
+    eat() {
+        console.log("狗吃饭");
+    }
+}
+
+class Factory3 {
+    create(val) {
+        switch (val) {
+            case("Cat"):
+                return new Cat(val);
+            case("Dog"):
+                return new Dog(val);
+        }
+        return {};
+    }
+}
+
+let factory3 = new Factory3();
+console.log(factory3.create('Cat'));
+console.log(factory3.create('Dog'));
+factory3.create('Cat').eat();
+factory3.create('Dog').eat();
+
--- wulitian-study-web-architecture/wulitian-study-design-pattern/创建型模式/02工厂方法模式/factory-method-mode.js
+++ wulitian-study-web-architecture/wulitian-study-design-pattern/创建型模式/02工厂方法模式/factory-method-mode.js
@@ -2,31 +2,93 @@
  * 工程方法模式示例
  */
 
-function Factory(type,content){
+/*-----------------------------------------*/
+function Factory(type, content) {
     if (this instanceof Factory) {
-        console.log(2)
+        console.log(2);
         return new this[type](content);
-    }else{
-        console.log(1)
-        return new Factory(type,content)
+    } else {
+        console.log(1);
+        return new Factory(type, content);
     }
 }
+
 Factory.prototype = {
-    javascript: function(content){
+    javascript: function (content) {
         console.log(content);
     },
-    java: function(content){
+    java: function (content) {
         console.log(content);
     },
-    vb: function(content){
+    vb: function (content) {
         console.log(content);
     }
 }
 const arr = [
-    {type:'javascript',content:'javascript我最强'},
-    {type:'java',content:'java我最强'},
-    {type:'vb',content:'vb我最强'}
+    {type: 'javascript', content: 'javascript我最强'},
+    {type: 'java', content: 'java我最强'},
+    {type: 'vb', content: 'vb我最强'}
 ]
-arr.forEach((e)=>{
-    Factory(e.type,e.content)
+arr.forEach((e) => {
+    Factory(e.type, e.content);
 })
+
+/*-----------------------------------------*/
+function car(val) {
+    this.name = val;
+    this.run = () => {
+        console.log(this.name);
+    }
+}
+
+function airplane(val) {
+    this.name = val;
+    this.air = () => {
+        console.log(this.name);
+    }
+}
+
+function Factory2(constructor, val) {
+    return new constructor(val);
+}
+
+let carInstance = new Factory2(car, '汽车');
+let airplaneInstance = new Factory2(airplane, '飞机');
+console.log(carInstance);
+console.log(airplaneInstance);
+carInstance.run();
+airplaneInstance.air();
+
+
+/*-----------------------------------------*/
+class cat {
+    constructor() {
+        this.name = "猫";
+    }
+
+    eat() {
+        console.log("猫吃饭");
+    }
+}
+
+class dog {
+    constructor() {
+        this.name = "狗";
+    }
+
+    eat() {
+        console.log("狗吃饭");
+    }
+}
+
+class Factory3 {
+    create(constructor) {
+        return new constructor();
+    }
+}
+
+let factory3 = new Factory3();
+console.log(factory3.create(cat));
+console.log(factory3.create(dog));
+factory3.create(cat).eat();
+factory3.create(dog).eat();
