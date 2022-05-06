454d40aea3526c54df0f0b27ecfe6e6c717df09f
wulitian <30992471+wulitian@users.noreply.github.com>
2022-05-06 09:35:29 +0800

    Update index.js

--- wulitian-study-web-front-end/01web前端知识/03js知识/consolelog/index.js
+++ wulitian-study-web-front-end/01web前端知识/03js知识/consolelog/index.js
@@ -1,1 +1,13 @@
+// 方法一
+// const log = console.log;
+// console.log = function () {
+//     log('自定义', ...arguments)
+// }
+// 方法二
+console.log=(function (oriLogFunc) {
+    return function () {
+        oriLogFunc('自定义',...arguments)
+    }
+})(console.log)
 
+console.log(1, 2)
